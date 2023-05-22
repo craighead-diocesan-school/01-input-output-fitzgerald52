991bd60e90fd205785f7e4a67c201146ffc2746f
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-05-23 10:42:35 +1200

    i added a joke

--- code/0-greeting.js
+++ code/0-greeting.js
@@ -3,11 +3,14 @@
 // ####################################
 
 function greeting() {
-  let username = prompt("What's your name?")
+  let username = prompt("What's your favourite colour?")
 
-  alert('Hello ' + username)
+  alert('wow your favourite colour is ' + username)
 }
 
 function joke() {
   // write the code for your joke here.
+  let joke = prompt ('do you want to hear a joke?')
+  alert('do you know what is the hardest part about eating a clock?')
+  alert(' It is really time consuming')
 }
