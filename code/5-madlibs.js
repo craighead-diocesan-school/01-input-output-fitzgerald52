528a398471951ee948daefe499ee608aa5468a53
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-05-29 09:56:20 +1200

    i made the program ask for nouns verbs ect and then add them into an alert that showcases the mad lib

--- code/5-madlibs.js
+++ code/5-madlibs.js
@@ -3,5 +3,9 @@
 // ####################################
 
 function madLibs() {
-  
+let exlimation = prompt ('Give me an exclimation')
+let adverb = prompt ('Give me an adverb')
+let noun = prompt ('Give me a noun')
+let adjective = prompt ('Give me an adjective ')
+let madlib = alert( exlimation + '! he said ' + adverb + ' as he jumped into his convertible ' + noun + ' and drove off with his ' + adjective + ' wife' )
 }
