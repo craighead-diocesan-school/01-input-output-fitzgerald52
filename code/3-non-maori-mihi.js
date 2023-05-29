12204016cc790358705c6877c62de78dfc8da627
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-05-29 12:03:37 +1200

    i made a program that helps user create a mihi

--- code/3-non-maori-mihi.js
+++ code/3-non-maori-mihi.js
@@ -7,13 +7,21 @@ function nonMaoriMihi() {
   alert('This program will help you build a basic mihi.')
   alert("First, you'll be asked a series of questions, then your mihi will be ready.")
 
-  alert('What is your name?')
-  let name = prompt()
-  // more questions and answers here
+  let name = prompt('What is your name?')
+
+  let mountain = prompt('what is your mountain')
+
+  let river = prompt('What is your body of water ?')
+
+  let hometown = prompt('What is your home town?')
 
   alert('Your mihi:')
-  // more outputs here
-
+  alert('Tēnā koutou,')
+  alert('Kei te mihi au ki a ' + mountain + ',') 
+  alert('Kei te mihi au ki a ' + river + ',') 
+  alert('Ko Te Tiriti o Waitangi te waka,') 
+  alert('Ko Aotearoa te iwi,') 
+  alert('Nō ' + hometown + ' au,') 
   alert('Ko ' + name + ' tōku ingoa,')
   alert('Tēnā koutou,')
   alert('Tēnā koutou,')
